7f664d939d6b62c0a7b05ae768fbdbbfb092ad87
Ajinkya <ajinkyabhakare1189@gmail.com>
2020-02-03 08:47:35 +0530

    second commit in new branch

--- src/gitsample/Demo2.java
+++ src/gitsample/Demo2.java
@@ -4,7 +4,8 @@ public class Demo2 {
 
 	public static void main(String[] args) {
 		
-		System.out.println("here is second file come from tester side");
+		System.out.println("this is demo 2");
+
 	}
 
 }
